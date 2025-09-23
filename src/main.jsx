a2f8cb1756c09e16f717976810c16d379462361a
Prajwal8173 <wakulkarprajwal987@gmail.com>
2025-09-23 18:25:44 +0530

    another change

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './index.css'
-import App from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "./index.css";
+import App from "./App.jsx";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApiProvider } from "./ApiContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+// ✅ Import interceptor
+import { enableAxiosInterceptor } from "./axiosInterceptor.js";
+
+// Enable API interceptor globally
+enableAxiosInterceptor(3000, (url, data) => {
+  console.log("📢 Updated from API:", url, data);
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <ApiProvider>
     <App />
-  </StrictMode>,
-)
+    </ApiProvider>
+  </StrictMode>
+);
